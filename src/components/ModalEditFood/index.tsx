47b0e71a937623b37410c9db0018ea385b823ac6
Pedro <pedro.amora@berrytech.com.br>
2021-03-17 14:44:32 -0300

    frist commit

---
+++ src/components/ModalEditFood/index.tsx
@@ -0,0 +1,55 @@
+import { FiCheckSquare } from "react-icons/fi";
+
+import { Form } from "./styles";
+import { Modal } from "../Modal";
+import { Input } from "../Input";
+
+interface FoodProps {
+  id: number;
+  name: string;
+  description: string;
+  price: string;
+  available: boolean;
+  image: string;
+}
+
+interface ModalEditFoodProps {
+  isOpen: boolean;
+  setIsOpen: () => void;
+  editingFood: FoodProps;
+  handleUpdateFood: (food: FoodProps) => void;
+}
+
+export function ModalEditFood({
+  setIsOpen,
+  editingFood,
+  handleUpdateFood,
+  isOpen,
+}: ModalEditFoodProps) {
+  const handleSubmit = async (data: FoodProps) => {
+    handleUpdateFood(data);
+    setIsOpen();
+  };
+
+  // ref={formRef}
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form onSubmit={handleSubmit} initialData={editingFood}>
+        <h1>Editar Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
+
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
+
+        <Input name="description" placeholder="Descrição" />
+
+        <button type="submit" data-testid="edit-food-button">
+          <div className="text">Editar Prato</div>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+}
